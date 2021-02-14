98935920ca64f1f51fa4375798efd5e582c1a880
assasin <assasin0308@sina.com>
2021-02-14 17:39:08 +0800

    Goroutine

--- test.go
+++ test.go
@@ -1,9 +1,20 @@
 package main
 
-func main() {
-	
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	go hello() //启动子 Goroutine
+	fmt.Println("main......")
+	time.Sleep(1*time.Second)
 }
+
+func hello() {
+	fmt.Println("这是一个函数,在另外一个Goroutine中执行")
+}
+
 
 
 
